13eeeb4308a1923734a463bde1eb4d93a853b4e3
Hans Jakob Håland <hansjhaa@stud.ntnu.no>
2022-02-15 13:10:48 +0100

    #3 Added tests Co-authored by Kristin <krshol@stud.ntnu.no>, Tage <tagemb@stud.ntnu.no>

--- what2eat/user/tests.py
+++ what2eat/user/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from .models import User
 
 # Create your tests here.
+class UserTestCase(TestCase):
+    def setUp(self):
+        User.objects.create(fname="first", lname="last")
+
+    def test_user_exist(self):
+        self.assertTrue(User.objects.filter(fname="first").exists())
+        self.assertFalse(User.objects.filter(fname="second").exists())
+
+    def test_user_id(self):
+        user = User.objects.get(id=1)
+        self.assertEqual(user.fname, "first")
+        self.assertEqual(user.lname, "last")
+
+        
+        
