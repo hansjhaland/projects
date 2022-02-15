5ffc02a1b38f404a2783d50878c51659df8b6531
Kristin Holter-Sørensen <kristinshs@dhcp-10-22-5-230.wlan.ntnu.no>
2022-02-15 15:27:55 +0100

    #3 new css for base co-author Johan <johagrot@stud.ntnu.no>

--- what2eat/user/models.py
+++ what2eat/user/models.py
@@ -8,7 +8,7 @@ from django.forms import ModelForm
 
 class User(models.Model):
     username = models.CharField(max_length=200)
-    # password = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
     fname = models.CharField('First name', max_length=200)
     lname = models.CharField('Last name',max_length=200)
     email = models.CharField(max_length=200)
