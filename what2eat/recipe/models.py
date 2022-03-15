48c41de164bde6ad3e25e6afda2ec7c68105da9e
Mia Bjørndal <miabjo@stud.ntnu.no>
2022-03-15 15:57:22 +0100

    Created Rating model

--- what2eat/recipe/models.py
+++ what2eat/recipe/models.py
@@ -1,3 +1,4 @@
+from asyncio.windows_events import NULL
 from tkinter import HIDDEN, Widget
 from turtle import title
 from unicodedata import category
@@ -5,8 +6,10 @@ from django.db import models
 from django import forms
 from django.forms import ModelForm
 from user.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 CHOICES = [("breakfast", "Frokost"),("lunch", "Lunsj"), ("dinner", "Middag")]
+RATING = [(1, "1"), (2, "2"), (3, "3"), (4, "4"), (5, "5")]
 
 # Create your models here.
 class Recipe(models.Model):
@@ -21,7 +24,16 @@ class Recipe(models.Model):
     def __str__(self):
         return self.title + self.category
 
+class Rating(models.Model):
+    rating = models.IntegerField(validators = [MinValueValidator(1), MaxValueValidator(5)])
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, null=False, default=1)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+    
 
+    class Meta:
+        unique_together = (("user", "recipe"),)
+        
+        
 class RecipeForm(ModelForm):
     class Meta:
         model = Recipe
@@ -35,6 +47,9 @@ class RecipeForm(ModelForm):
 class categoryForm(forms.Form):
     CHOICES.insert(0,("all", "Alle"))
     option = forms.ChoiceField(choices=CHOICES, label="", widget=forms.RadioSelect)
+
+class RatingForm(forms.Form):
+    option = forms.ChoiceField(choices=RATING, label="", widget=forms.RadioSelect)
 
 
 
